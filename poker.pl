a907109e92b3748d7c05494b5e285060234e036b
noperative <noperative.rx@gmail.com>
2017-10-23 21:31:13 -0700

    set layout for the basic game

--- poker.pl
+++ poker.pl
@@ -3,12 +3,24 @@
 % ! Poker (H, K) where H is a list of 5 cards and K is a list of =< 5
 % cards to keep from H in order to maximize your win
 % TODO: Actually make it
-poker(H, K):-.
+poker(H, K):-
+    valid_hand(C),
+    find_best_combo(H, C),
+    common(H, C, K).
 
+% valid_hand(H) is only true if there is a valid hand
+valid_hand([H|T]):-
+    \+ duplicates([H|T]),
+    valid_card(T).
+
+% common(H, C, K) returns the common cards K between H and C
+
+% find_best_combo(H, C) finds the best combo C from hand H
+% TODO: actually do it
 
 
 %!Rules
-% TODO: be able to parse a list into each type
+% TODO: be able to parse a list into each type instead of a dumb string
 
 value("royal straight flush" , 250).
 value("five of a kind", 60).
