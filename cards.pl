383e20b52553a725e4cfe54e9842954e13fe7534
David Zhang <dzhangd95@gmail.com>
2017-10-23 22:24:11 -0700

    Add card definitions

---
+++ cards.pl
@@ -0,0 +1,11 @@
+% card(V, S) is true if V is a valid card value and S is a valid suit
+card(V, S) :-
+    number(V),
+    between(2, 10, V),
+    valid_suit(S).
+card(V, S) :-
+ member(V, [ace, jack, queen, king]),
+ valid_suit(S).
+
+valid_suit(S) :-
+    member(S, [spades, hearts, clubs, diamonds]). 
