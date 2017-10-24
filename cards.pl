9b2ba56e779d9663d2a86a60e31a1a5155106794
noperative <noperative.rx@gmail.com>
2017-10-24 16:43:18 -0700

    fixed number enumeration bug

--- cards.pl
+++ cards.pl
@@ -2,7 +2,6 @@
 
 % card(V, S) is true if V is a valid card value and S is a valid suit
 card(V, S) :-
-    number(V),
     between(2, 10, V),
     valid_suit(S).
 card(V, S) :-
@@ -10,4 +9,4 @@ card(V, S) :-
  valid_suit(S).
 
 valid_suit(S) :-
-    member(S, [spades, hearts, clubs, diamonds]). 
+    member(S, [spades, hearts, clubs, diamonds]).
